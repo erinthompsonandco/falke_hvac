6705e622e511e3a0d6fcd50867d06de2daace19c
Erin Thompson <erinthompsonandco@yahoo.com>
2015-11-05 20:12:17 -0800

    Some basic setup of the UI with buttons that transition to differnet XML activities and inflate differnt XML layout screens. TODO: make the appropriate changes to the input screen, make it save to an onboard database, and make it transmit to a server side database.

---
+++ src/main/java/com/branches/thompson/erin/tablet_to_server/userOpen.java
@@ -0,0 +1,13 @@
+package com.branches.thompson.erin.tablet_to_server;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class userOpen extends Activity {
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.open_layout);
+    }
+
+}
---
+++ src/main/java/com/branches/thompson/erin/tablet_to_server/userOptions.java
@@ -0,0 +1,13 @@
+package com.branches.thompson.erin.tablet_to_server;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class userOptions extends Activity {
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.options_layout);
+    }
+
+}
